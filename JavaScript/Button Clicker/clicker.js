e0667d22a6a95fb90d09a04a8b7d120ba44bd9c2
maramnaqeeb <maramnaqib1998@gmail.com.code>
2022-09-03 15:38:58 +0300

    Button Clicker

---
+++ JavaScript/Button Clicker/clicker.js
@@ -0,0 +1,9 @@
+function logout(element) {
+  element.innerText = "logout";
+}
+function hide(element) {
+  element.remove();
+}
+function show() {
+  alert("You clicked on the button, so it triggered the alert box");
+}
