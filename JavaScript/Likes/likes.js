0a5a88bc1e4781be8200ad339792924dffb10a5d
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-09-06 14:12:01 +0300

    likes

--- JavaScript/Likes/likes.js
+++ JavaScript/Likes/likes.js
@@ -1,4 +1,15 @@
-function incrementValue(){
-    var counter=document.querySelector("#like1")
-    counter.innerText=20;}
+function incrementValue1(){
+    var element=document.querySelector("#like1");
+    element.innerText++;
+}
 
+function incrementValue2(){
+    var element=document.querySelector("#like2");
+    element.innerText++;
+}
+
+
+function incrementValue3(){
+    var element=document.querySelector("#like3");
+    element.innerText++;
+}
